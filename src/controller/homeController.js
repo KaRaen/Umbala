cb2be47df751ac3d2fc5a3450351a58f20b6313e
unknown <vinhtruong311@gmail.com>
2022-11-20 16:52:07 +0700

    thanh nav_2

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -10,22 +10,25 @@ let getHomepage = (req, res) => {
         'SELECT * FROM `users`',
         function (err, results, fields) {
             console.log('>>> check mysql')
-            console.log(results); // results contains rows returned by server
+            // console.log(results); // results contains rows returned by server
             results.map((row) => {
                 data.push({
                     id: row.id,
                     email: row.email,
                     address: row.address,
                     lastname: row.lastname,
-                    lastname: row.lastname
+                    firstname: row.firstname
 
                 })
             });
-            return res.render('index.ejs', { dataUser: JSON.stringify(data) })
+
+            return res.render('index.ejs', { dataUser: data, test: ' ahahha ' }) // chi dinh file view , tham so {} kieu object key: value
+
         })
 
 
 }
+
 module.exports = {
     getHomepage
 }
